408e666d7db93aa75324f4bc91eb2745b4efb0c5
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-10-04 20:05:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const API_KEY = "e467712b257e418838be97cc881a71de";
+const API_KEY = "086d9464daddd3ef42fa4f97bcac9b6f";
 
 function App() {
   const [query, setQuery] = useState("");
